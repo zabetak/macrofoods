7faeeedaa0aed34a57d1b77d7b6ba3e48913de1d
zabetak <zabetak@stamatiss-imac.home>
2018-02-11 23:15:48 +0100

    Adds a servlet listener for registering the EMF in the context.

--- backend/src/main/java/org/macrofoods/backend/servlets/ContextListener.java
+++ backend/src/main/java/org/macrofoods/backend/servlets/ContextListener.java
@@ -6,15 +6,15 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public final class ContextListener implements ServletContextListener {
+	static final String EM_FACTORY_ATTR = "EMF";
 
 	public void contextInitialized(ServletContextEvent evt) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRM");
-		evt.getServletContext().setAttribute(EMServlet.EM_FACTORY_ATTR, emf);
+		evt.getServletContext().setAttribute(EM_FACTORY_ATTR, emf);
 	}
 
 	public void contextDestroyed(ServletContextEvent evt) {
-		EntityManagerFactory emf = (EntityManagerFactory) evt.getServletContext()
-				.getAttribute(EMServlet.EM_FACTORY_ATTR);
+		EntityManagerFactory emf = (EntityManagerFactory) evt.getServletContext().getAttribute(EM_FACTORY_ATTR);
 		if (emf != null)
 			emf.close();
 		// TODO Else log appropriate
