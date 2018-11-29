7e3930051c764a1e298cdf8c413f249db6fd070b
zabetak <15013153+zabetak@users.noreply.github.com>
2018-11-30 00:22:29 +0100

    Improve ingredients component to manage groups of ingredients

---
+++ frontend/src/app/ingredient-group.ts
@@ -0,0 +1,38 @@
+import { Food } from './food';
+import { Ingredient } from './ingredient';
+
+export class IngredientGroup {
+  id: number;
+  name: string = "Ingredients";
+  seq: number;
+  ingredients: Ingredient[] = [];
+  calTotal: number = 0;
+  protTotal: number = 0;
+  carbTotal: number = 0;
+  fatTotal: number = 0;
+
+  add(iFood: Food){
+    let ing: Ingredient = {
+        id: -1,
+        seq: this.ingredients.length,
+        amount: 100,
+        food: iFood
+    };
+    this.ingredients.push(ing);
+    this.calculateSums();
+  }
+
+  calculateSums(): void {
+    this.calTotal = 0;
+    this.protTotal = 0;
+    this.carbTotal = 0;
+    this.fatTotal = 0;
+
+    this.ingredients.forEach((ing:Ingredient) => {
+        this.calTotal += ing.food.kcal * (ing.amount/100);
+        this.protTotal += ing.food.protein * (ing.amount/100) ;
+        this.carbTotal += ing.food.carbohydrate * (ing.amount/100);
+        this.fatTotal += ing.food.fat * (ing.amount/100);
+    });
+  }
+}
--- frontend/src/app/ingredients/ingredients.component.ts
+++ frontend/src/app/ingredients/ingredients.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../ingredient';
 import { Food } from '../food';
+import { IngredientGroup } from '../ingredient-group';
 
 
 
@@ -10,12 +11,7 @@ import { Food } from '../food';
   styleUrls: ['./ingredients.component.css']
 })
 export class IngredientsComponent implements OnInit {
-
-  ingredients: Ingredient[] = [];
-  calTotal: number = 0;
-  protTotal: number = 0;
-  carbTotal: number = 0;
-  fatTotal: number = 0;
+  groups: IngredientGroup[] = [];
 
   constructor() { }
 
@@ -25,29 +21,12 @@ export class IngredientsComponent implements OnInit {
   @Input()
   set food(iFood: Food) {
       if(iFood != null){
-        let ing: Ingredient = {
-            id: -1,
-            seq: this.ingredients.length,
-            amount: 100,
-            food: iFood
-        };
-        this.ingredients.push(ing);
-        this.calculateSums();
+        this.groups[this.groups.length-1].add(iFood);
       }
   }
 
-  calculateSums(): void {
-    this.calTotal = 0;
-    this.protTotal = 0;
-    this.carbTotal = 0;
-    this.fatTotal = 0;
-
-    this.ingredients.forEach((ing:Ingredient) => {
-        this.calTotal += ing.food.kcal * (ing.amount/100);
-        this.protTotal += ing.food.protein * (ing.amount/100) ;
-        this.carbTotal += ing.food.carbohydrate * (ing.amount/100);
-        this.fatTotal += ing.food.fat * (ing.amount/100);
-    });
+  addGroup(): void {
+    this.groups.push(new IngredientGroup());
   }
 
 }
