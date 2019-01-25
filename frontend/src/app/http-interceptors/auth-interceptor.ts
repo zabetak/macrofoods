0ad923a4da69bd7fa7a5e2c06a34f37118c2ff72
zabetak <15013153+zabetak@users.noreply.github.com>
2019-01-26 00:57:39 +0100

    Change interceptor to not modify request if there is no token

--- frontend/src/app/http-interceptors/auth-interceptor.ts
+++ frontend/src/app/http-interceptors/auth-interceptor.ts
@@ -14,12 +14,13 @@ export class AuthInterceptor implements HttpInterceptor {
     // Get the auth token from the service.
     const authToken = this.ses.getValidTokenOrNull();
 
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', authToken)
-    });
-    
-    // send cloned request with header to the next handler.
-    return next.handle(authReq);
+    if(authToken != null){
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', authToken)
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
   }
 }
 
